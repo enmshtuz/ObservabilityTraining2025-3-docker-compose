5bdcbbde02a165d405790223affc35e6366439d6
enmshtuz <marvardyan@gmail.com>
2025-03-20 14:29:43 +0400

    Supply default values for env vars in docker-compose.yml

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 DB_USER = os.getenv("POSTGRES_USER")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-DB_NAME = os.getenv("POSTGRES_DB", "testdb")
-DB_HOST = os.getenv("DB_HOST", "db")
-DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 
 def is_ready():
     try:
